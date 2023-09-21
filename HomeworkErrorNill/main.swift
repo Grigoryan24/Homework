179ff193e55b3366b7d7995534f780c00d62fd15
Grigoryan24 <grgrn.art@mail.ru>
2023-09-21 21:19:05 +0300

    Домашняя работа 1

--- HomeworkErrorNill/main.swift
+++ HomeworkErrorNill/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+// Вклад
+var vklad: Int = 1000
 
+// Годовой процент
+var percent: Int = 5
+
+// Найти сумму вклада через 5 лет
+var years: Int = 5
+
+var otvet = (vklad * (Int(Double(percent)) * years ) / 100)
+print(otvet)
+
+// Формула
+// (1000 * 0.5 * 5) / 100
